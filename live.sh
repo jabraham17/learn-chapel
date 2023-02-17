9384bb2afa718f9d43e99e858410c9c13211c579
Jade Abraham <jade.abraham@hpe.com>
2023-02-17 12:00:57 -0800

    update live script
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- live.sh
+++ live.sh
@@ -5,9 +5,9 @@ compile_and_run() {
     filename=$(basename $1)
     dir=$(dirname $1)
     execname=live.out
-    (cd $dir && set -x && chpl $filename -o $execname && ./$execname)
+    (cd $dir && echo "" && set -x && chpl $filename -o $execname && ./$execname)
 }
 
 
 f=$1
-fswatch -o $f | (while read; do compile_and_run $f || true; done)
+fswatch -o $f -n1 | (while read; do compile_and_run $f || true; done)
